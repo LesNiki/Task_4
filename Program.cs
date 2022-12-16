70d7a24bbe6331da133ac0efe8b242b92e80eb66
olesia <la@mail.ru>
2022-12-16 16:06:08 +0300

    MaxFrom3

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая запрашивает число и выдает его квадрат
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int sqt = number*number;
+
+Console.WriteLine(sqt);
