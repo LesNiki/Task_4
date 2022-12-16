2f8ee05d11e0224dbc3f47d47196517c67169430
LesNiki <120560252+LesNiki@users.noreply.github.com>
2022-12-16 16:07:58 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Напишите программу, которая запрашивает число и выдает его квадрат
-
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32 (Console.ReadLine());
-
-int sqt = number*number;
-
-Console.WriteLine(sqt);
